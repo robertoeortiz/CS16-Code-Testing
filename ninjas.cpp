97e2040d3ea463321bf1297fbf5aa50a3ecf6744
Roberto Ortiz <robertoortiz@ucsb.edu>
2025-10-16 20:59:31 -0700

    Adjust ninja placement logic in ninjas.cpp

--- ninjas.cpp
+++ ninjas.cpp
@@ -14,18 +14,14 @@ void printNinjasPositions(int size)
     return;
   }
 
-  int ninjaCount;
-
-  for(int i = 1; i < size - 1; i++) //for each floor combination 
+  for(int i = 1; i < size - 3; i++) //for each floor combination 
   {
     string floor(size, TILE);
     floor[i] = NINJA;
-    ninjaCount = 1;
 
     for(int j = i + 2; j < size -1; j++) //for every 2nd ninja
     {
       floor[j] = NINJA;
-      ninjaCount++;
       cout << floor << endl;
     }
   }
